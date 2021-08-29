431b2753968ac318ca13df0fe883cfc438ff4dcd
dclemon <63597032+dclemon@users.noreply.github.com>
2021-08-29 18:26:12 +0800

    Update qhb.js

--- qhb.js
+++ qhb.js
@@ -16,144 +16,115 @@ hostname = api2.guaniuvideo.com
 #####################################################################################################################################
 */
 
-const $ = new Env('趣红包');
+const $ = new Env("趣头条阅读文章")
+
+let Starturl = [], gainscore = Number(),Starthd = [];
+let urlArr = [],hdArr = [];
+let startbodys = $.getdata('qtt_start')
+let endbodys = $.getdata('qtt_end')
+
+//这边是临时写的环境，放到圈×里之前要删掉
+
+
+//以下是主体代码
+
+
+
+
 !(async () => {
     if (typeof $request !== "undefined") {
-        await qhbck()
+        await qttck()
 
     } else {
-        
-        var num = 1;
-        while (num<=9){
-            console.log(`\n趣红包观看视频！💦\n`)
-            await qhb_spjb()
-            await $.wait(60000);
-            num++;
-            
+
+        console.log(`\n趣头条開始！💦\n`)
+        Object.keys(Starturl).forEach((item) => {
+            if (Starturl[item]) {
+                urlArr.push(Starturl[item])
+
+            }
+        })
+
+        Object.keys(Starthd).forEach((item) => {
+            if (Starthd[item]) {
+                hdArr.push(Starthd[item])
+
+            }
+        })
+        console.log(`您共提供${urlArr.length}次阅读任务`)
+        for (let i = 0; i < urlArr.length; i++) {
+            if (urlArr[i]) {
+                gainurl = urlArr[i];
+                gainhd = JSON.parse(hdArr[i])
+                $.index = i + 1;
+                console.log(`-------------------------\n\n开始趣头条第${$.index}次任务`)
+            }
+            await qtt_read();
         }
-        
-        console.log(`\n趣红包离线奖励！💦\n`)
-        //await qhb_lxjl()
-        $.msg("","","趣红包运行完毕！")
-        $.done();
+        console.log(`-------------------------\n\n趣头条共完成${$.index}次任务，浏览赚任务全部结束`);
+        $.msg($.name, `共完成${$.index}次任务`)
     }
 })()
     .catch((e) => $.logErr(e))
     .finally(() => $.done())
 
 
-//数据获取
-function qhbck() {
+
+
+
+
+function qtt_read() {
+    return new Promise((resolve, reject) => {
+        let url = {
+            url: gainurl,
+            headers: gainhd,
+        };
+        $.get(url, async(error, response, data) => {
+            try{
+                let startres = JSON.parse(data);
+                if(startres.code == 0){
+                    $.log("阅读请求成功，获得"+startres.items.data.curr_task.amount+'金币')
+                }else if(startres.code == -1803){
+                    $.log("失败:"+startres.message)
+                }else{
+                    $.log("失败:"+startres.message)
+                }
+                await $.wait(2000);
+                resolve()
+            }catch(e){}finally {resolve()}
+        })
+    })
+}
+
+function qttck(){
     console.log($request.url)
     
-    if ($request.url == "http://api2.guaniuvideo.com/reward/video") {
-        const qhburl = $request.url
-        if (qhburl) $.setdata(qhburl, 'qhburl')
-        $.log(qhburl)
-        const qhbhd = JSON.stringify($request.headers)
-        if (qhbhd) $.setdata(qhbhd, 'qhbhd')
-        $.log(qhbhd)
-        const qhbbody = JSON.stringify($request.body)
-        if (qhbbody) $.setdata(qhbhd, 'qhbbody')
-        $.log(qhbbody)
+    if ($request.url.indexOf("report") > -1){
+
+
+        if ($.getdata('qtt_url') == 'undifined'){
+            const qtturl = $request.url
+            if(qtturl)     $.setdata(qtturl+'&','qtt_url')
+            const qtthd = JSON.stringify($request.headers)
+            if(qtthd)    $.setdata(qtthd+'&'+h1,'qtt_hd')
+            $.msg($.name,"","趣头条第一次数据获取成功！")
+
+
+        }else{
+            const qtturl = $request.url
+            u1 = $.getdata('qtt_url')
+            if(qtturl)     $.setdata(qtturl+'&'+u1,'qtt_url')
+            const qtthd = JSON.stringify($request.headers)
+            h1 = $.getdata('qtt_hd')
+            if(qtthd)    $.setdata(qtthd+'&'+h1,'qtt_hd')
+            $.msg($.name,"","趣头条数据获取成功！")
+            
+            
+        }
+
         
-        
-        $.msg($.name, "", "趣红包视频数据获取成功！")
-    }
-    else if ($request.url == "http://api2.guaniuvideo.com/reward/videoNotify") {
-        const qhb_gg_url = $request.url
-        if (qhb_gg_url) $.setdata(qhb_gg_url, 'qhb_gg_url')
-        $.log(qhb_gg_url)
-        const qhb_gg_hd = JSON.stringify($request.headers)
-        if (qhb_gg_hd) $.setdata(qhb_gg_hd, 'qhb_gg_hd')
-        $.log(qhb_gg_hd)
-        const qhb_gg_body = JSON.stringify($request.body)
-        if (qhb_gg_body) $.setdata(qhb_gg_body, 'qhb_gg_body')
-        $.log(qhb_gg_body)
-        
-        
-        $.msg($.name, "", "趣红包广告数据获取成功！")
-    }
-
-    else if ($request.url == "http://api2.guaniuvideo.com/index/leaveReward") {
-        console.log('离线数据匹配成功')
-        const qhb_lx_url = $request.url
-        if (qhb_lx_url) $.setdata(qhb_lx_url, 'qhb_lx_url')
-        $.log(qhb_lx_url)
-        const qhb_lx_hd = JSON.stringify($request.headers)
-        if (qhb_lx_hd) $.setdata(qhb_lx_hd, 'qhb_lx_hd')
-        $.log(qhb_lx_hd)
-        
-        
-        $.msg($.name, "", "趣红包离线奖励数据获取成功！")
-    }
-}
-
-
-//视频金币
-function qhb_spjb(timeout = 1000) {
-
-  return new Promise((resolve) => {
-    setTimeout( ()=>{
-      if (typeof $.getdata('qhbhd') === "undefined") {
-        $.msg($.name,"",'请先获取趣红包视频数据',)
-        $.done()
-      }
-    qhbhd = JSON.parse($.getdata('qhbhd')) 
-    let url = {
-    url: $.getdata('qhburl'),
-    method: `POST`,
-    headers: qhbhd,
-    body: $.getdata('qhbbody')
-    }
-    $.post(url, async (err, resp, data) => {
-        try{
-            const result = JSON.parse(data)
-            if(result.code == 200){
-                console.log('趣红包观看成功：获得'+String(result.data.reward_gold)+"金币。")
-            }
-            else{
-                console.log('出错了，以下是详细信息：'+String(result.data))
-            }
-        }catch(e){}finally{resolve()}
-    })
-    },timeout)
-  })
-}
-
-//离线奖励，似乎一天之内领5次，暂时ban了
-function qhb_lxjl(timeout = 1000) {
-
-  return new Promise((resolve) => {
-    setTimeout( ()=>{
-      if (typeof $.getdata('qhb_lx_hd') === "undefined") {
-        $.msg($.name,"",'请先获取趣红包离线奖励数据',)
-        $.done()
-      }
-
-
-    qhb_lx_hd = JSON.parse($.getdata('qhb_lx_hd')) 
-    let url = {
-    url: $.getdata('qhb_lx_url'),
-    method: `POST`,
-    headers: qhb_lx_hd,
-    body: ''
-    }
-    $.post(url, async (err, resp, data) => {
-        try{
-            const result = JSON.parse(data)
-            if(result.code == 200){
-                console.log('趣红包离线奖励领取成功：获得'+String(result.data.reward_gold)+"金币。")
-            }
-            else{
-                console.log('出错了，以下是详细信息：'+String(result.data))
-            }
-        }catch(e){}finally{resolve()}
-    })
-    },timeout)
-  })
-}
-    
+        }
+}  
     
 
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
